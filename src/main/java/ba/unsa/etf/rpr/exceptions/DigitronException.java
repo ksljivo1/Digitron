d0454e7fb4b7d27d1c25533f43210a8788ff1d3f
ksljivo1 <ksljivo1@etf.unsa.ba>
2022-12-28 21:34:46 +0100

    Kreirana klasa - izuzetak DigitronException

---
+++ src/main/java/ba/unsa/etf/rpr/exceptions/DigitronException.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.exceptions;
+
+public class DigitronException extends Exception {
+    public DigitronException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DigitronException(String message) {
+        super(message);
+    }
+}
