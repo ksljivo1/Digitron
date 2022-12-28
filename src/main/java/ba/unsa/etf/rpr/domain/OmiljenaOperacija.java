6eaa3edd2a117e5f807fc7072076c42af48d0953
ksljivo1 <ksljivo1@etf.unsa.ba>
2022-12-28 21:16:39 +0100

    Kreirana klasa OmiljenaOperacija

---
+++ src/main/java/ba/unsa/etf/rpr/domain/OmiljenaOperacija.java
@@ -0,0 +1,69 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class OmiljenaOperacija implements Idable {
+    private int id;
+    private String operacija;
+    private int brojPonavljanja;
+    private int idKorisnik;
+
+    public OmiljenaOperacija() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdKorisnik() {
+        return idKorisnik;
+    }
+
+    public void setIdKorisnik(int idKorisnik) {
+        this.idKorisnik = idKorisnik;
+    }
+
+    public String getOperacija() {
+        return operacija;
+    }
+
+    public void setOperacija(String operacija) {
+        this.operacija = operacija;
+    }
+
+    public int getBrojPonavljanja() {
+        return brojPonavljanja;
+    }
+
+    public void setBrojPonavljanja(int brojPonavljanja) {
+        this.brojPonavljanja = brojPonavljanja;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OmiljenaOperacija that = (OmiljenaOperacija) o;
+        return id == that.id && brojPonavljanja == that.brojPonavljanja && Objects.equals(operacija, that.operacija);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, operacija, brojPonavljanja);
+    }
+
+    @Override
+    public String toString() {
+        return "OmiljenaOperacija{" +
+                "id=" + id +
+                ", operacija='" + operacija + '\'' +
+                ", brojPonavljanja=" + brojPonavljanja +
+                '}';
+    }
+}
