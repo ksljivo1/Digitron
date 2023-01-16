3b3619ba0f835cac8eb0e09f99580ab6274f8e3f
ksljivo1 <ksljivo1@etf.unsa.ba>
2023-01-16 17:13:40 +0100

    Kreirana klasa AbstractDao

--- src/main/java/ba/unsa/etf/rpr/Digitron.java
+++ src/main/java/ba/unsa/etf/rpr/Digitron.java
@@ -1,7 +1,9 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.*;
+
 public class Digitron {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("test");
     }
 }
---
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -0,0 +1,169 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.DigitronException;
+
+import java.sql.*;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+    private Connection connection;
+    private String tableName;
+
+    public AbstractDao(String tableName) {
+        try {
+            this.tableName = tableName;
+            String url = "jdbc:mysql://sql.freedb.tech/freedb_DigitronBaza?serverTimezone=UTC";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(url, "freedb_ksljivo1", "Kp63vUJwc4*vN$C");
+        }
+        catch(Exception e) {
+            System.out.println("Nemoguce uspostaviti konekciju na bazu");
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection(){
+        return this.connection;
+    }
+
+    public abstract T row2object(ResultSet rs) throws DigitronException, SQLException;
+
+    public abstract Map<String, Object> object2row(T object);
+
+    public T getById(int id) throws DigitronException {
+        String query = "SELECT * FROM " + tableName + " WHERE id = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()) {
+                T result = row2object(rs);
+                rs.close();
+                return result;
+            }
+            else {
+                throw new DigitronException("Object not found");
+            }
+        }
+        catch(SQLException e) {
+            throw new DigitronException(e.getMessage(), e);
+        }
+    }
+
+    public List<T> getAll() throws DigitronException {
+        String query = "SELECT * FROM " + tableName;
+        List<T> results = new ArrayList<T>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+                T object = row2object(rs);
+                results.add(object);
+            }
+            rs.close();
+            return results;
+        }
+        catch(SQLException e) {
+            throw new DigitronException(e.getMessage(), e);
+        }
+    }
+
+    public void delete(int id) throws DigitronException {
+        String sql = "DELETE FROM " + tableName + " WHERE id = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, id);
+            stmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            throw new DigitronException(e.getMessage(), e);
+        }
+    }
+
+    private Map.Entry<String, String> prepareInsertParts(Map<String, Object> row) {
+        StringBuilder columns = new StringBuilder();
+        StringBuilder questions = new StringBuilder();
+        int counter = 0;
+        for(Map.Entry<String, Object> entry : row.entrySet()) {
+            counter = counter + 1;
+            if(entry.getKey().equals("id")) continue;
+            columns.append(entry.getKey());
+            questions.append("?");
+            if(row.size() != counter) {
+                columns.append(",");
+                questions.append(",");
+            }
+        }
+        return new AbstractMap.SimpleEntry<>(columns.toString(), questions.toString());
+    }
+
+    public T add(T item) throws DigitronException {
+        Map<String, Object> row = object2row(item);
+        Map.Entry<String, String> columns = prepareInsertParts(row);
+        StringBuilder builder = new StringBuilder();
+        builder.append("INSERT INTO ").append(tableName);
+        builder.append(" (").append(columns.getKey()).append(") ");
+        builder.append("VALUES (").append(columns.getValue()).append(")");
+        try {
+            PreparedStatement stmt = connection.prepareStatement(builder.toString(), Statement.RETURN_GENERATED_KEYS);
+            int counter = 1;
+            for (Map.Entry<String, Object> entry: row.entrySet()) {
+                if (entry.getKey().equals("id")) continue;
+                stmt.setObject(counter, entry.getValue());
+                counter = counter + 1;
+            }
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            item.setId(rs.getInt(1));
+            return item;
+        } catch(SQLException e){
+            throw new DigitronException(e.getMessage(), e);
+        }
+    }
+
+    private String prepareUpdateParts(Map<String, Object> row){
+        StringBuilder columns = new StringBuilder();
+
+        int counter = 0;
+        for(Map.Entry<String, Object> entry : row.entrySet()) {
+            counter = counter + 1;
+            if(entry.getKey().equals("id")) continue;
+            columns.append(entry.getKey()).append("= ?");
+            if(row.size() != counter) {
+                columns.append(",");
+            }
+        }
+        return columns.toString();
+    }
+
+    public T update(T item) throws DigitronException {
+        Map<String, Object> row = object2row(item);
+        String updateColumns = prepareUpdateParts(row);
+        StringBuilder builder = new StringBuilder();
+        builder.append("UPDATE ")
+                .append(tableName)
+                .append(" SET ")
+                .append(updateColumns)
+                .append(" WHERE id = ?");
+        try {
+            PreparedStatement stmt = connection.prepareStatement(builder.toString());
+            int counter = 1;
+            for(Map.Entry<String, Object> entry: row.entrySet()) {
+                if (entry.getKey().equals("id")) continue;
+                stmt.setObject(counter, entry.getValue());
+                counter = counter + 1;
+            }
+            stmt.setObject(counter, item.getId());
+            stmt.executeUpdate();
+            return item;
+        }
+        catch(SQLException e) {
+            throw new DigitronException(e.getMessage(), e);
+        }
+    }
+}
