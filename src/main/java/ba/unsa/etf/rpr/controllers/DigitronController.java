1b91948c8de758bce723b86a3cd1333a50948e90
ksljivo1 <ksljivo1@etf.unsa.ba>
2023-01-18 16:57:24 +0100

    Kreirana metoda evaluate koja racuna izraz

--- src/main/java/ba/unsa/etf/rpr/controllers/DigitronController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DigitronController.java
@@ -14,6 +14,60 @@ public class DigitronController {
     public Label display;
     public double rez = 0;
 
+    // prva verzija funkcije koja parsira izraz, zatim ga racuna / baca izuzetak ukoliko izraz nije valjan ili se javlja dijeljenje nulom
+    // podrzane su osnovne aritmeticke operacije (+, -, *, /)
+    // podrzan je prioritet izvrsavanja operacija
+    // nije podrzano koristenje zagrada
+
+    private static String evaluate(String izraz) {
+        try {
+            Double.parseDouble(izraz);
+            return izraz + ".0"; // ako je izraz vec broj, odmah se vraca
+        }
+        catch(Exception ignored) {
+
+        }
+
+        // lista koja sadrzi sve argumente osnovnih aritmetickih operacija koje se javljaju u izrazu
+        List<String> listaArgumenata = new ArrayList<>(Arrays.asList(izraz.split(" \\+ | \\* | \\- | \\/ ")));
+        // lista koja sadrzi sve osnovne aritmeticke operacije koje se javljaju u izrazu
+        List<String> listaOperacija = new ArrayList<>(Arrays.asList(Arrays.stream(izraz.split("[+-]?([0-9]*[.]?[0-9]+|[0-9]+[.]?[0-9]*)"))
+                                                                          .filter(s -> !(s.isBlank()))
+                                                                          .toArray(String[]::new)));
+
+        // posto su svi podrzani operatori binarni, valjan izraz ce uvijek imati broj operatora za jedan manji od broja argumenata
+        if(listaOperacija.size() >= listaArgumenata.size()) return "ERROR: Invalid syntax";
+        double rezultat = 0;
+        try {
+            for(int i = 0; i < listaArgumenata.size() - 1; i++) {
+                double op1 = Double.parseDouble(listaArgumenata.get(i));
+                if(i != 0) op1 = rezultat;
+                int t = i + 1;
+                while(t < listaOperacija.size() && (listaOperacija.get(t).trim().equals("*") || listaOperacija.get(t).trim().equals("/"))) {
+                    if(listaOperacija.get(t).trim().equals("*"))
+                        listaArgumenata.set(t,
+                                String.valueOf(Double.parseDouble(listaArgumenata.get(t)) * Double.parseDouble(listaArgumenata.get(t + 1))));
+                    else
+                        listaArgumenata.set(t,
+                                String.valueOf(Double.parseDouble(listaArgumenata.get(t)) / Double.parseDouble(listaArgumenata.get(t + 1))));
+                    listaOperacija.remove(t);
+                    listaArgumenata.remove(t + 1);
+                }
+                double op2 = Double.parseDouble(listaArgumenata.get(i + 1));
+                String znak = listaOperacija.get(i).trim();
+                if(znak.equals("+")) rezultat = (op1 + op2);
+                else if(znak.equals("-")) rezultat = (op1 - op2);
+                else if(znak.equals("*")) rezultat = (op1 * op2);
+                else rezultat = (op1 / op2);
+            }
+            if(Double.valueOf(rezultat).isInfinite() || Double.valueOf(rezultat).isNaN()) return "ERROR: Division by zero";
+            else return String.valueOf(rezultat);
+        }
+        catch (NumberFormatException e) {
+            return "ERROR: Invalid syntax";
+        }
+    }
+
     public void btn0Click(ActionEvent actionEvent) {
         String tekst = display.getText();
         if(tekst.equals("0") || tekst.contains("=")) display.setText("0");
@@ -120,6 +174,8 @@ public class DigitronController {
     }
 
     public void equalsBtnClicked(ActionEvent actionEvent) {
+        String tekst = display.getText();
+        display.setText(tekst + " = " + evaluate(tekst));
     }
 
     public void cBtnClicked(ActionEvent actionEvent) {
